af8a628299f640347b356ed177e74889195c7ef3
student-a502 <student-a502@STUD-RM19-A502>
2025-10-30 17:45:30 +0500

    разработан класс StudentList. Коневский

---
+++ Pr11/Classes/StudentList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr11.Classes
+{
+    internal class StudentList
+    {
+        private List<Student> students = new List<Student>();
+        public StudentList()
+        {
+            students.Add(new Student("Иванов Иван Иванович"));
+            students.Add(new Student("Сидоров Алексей Валерьевич"));
+            students.Add(new Student("Петрова Анна Сергеевна"));
+        }
+        public string DisplayStudents()
+        {
+            return DisplayStudents(",");
+        }
+        public string DisplayStudents(string separator)
+        {
+            var name = students.Select(s => s.FullName);
+            return string.Join(separator, name);
+        }
+        public List<Student> GetStudents()
+        {
+            return students;
+        }
+    }
+}
