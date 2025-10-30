3187e5469591f1dbbbf0fdb47516305d25b500bb
student-a502 <student-a502@STUD-RM19-A502>
2025-10-30 18:06:12 +0500

    Разработан метод вывода

--- Pr11/MainWindow.xaml.cs
+++ Pr11/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Pr11.Classes;
 
 namespace Pr11
 {
@@ -20,9 +22,28 @@ namespace Pr11
     /// </summary>
     public partial class MainWindow : Window
     {
+        private StudentList studentList;
         public MainWindow()
         {
             InitializeComponent();
+            studentList = new StudentList();
+            StudentListBox.ItemsSource = studentList.GetStudents();
+        }
+
+        private void CommaButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResultTextBlock.Text = studentList.DisplayStudents();
+        }
+
+        private void CustomButton_Click(object sender, RoutedEventArgs e)
+        {
+            string Separator = SeparatorTextBox.Text;
+            if (string.IsNullOrEmpty(Separator))
+            {
+                MessageBox.Show("Введите разделитель");
+                return;
+            }
+            ResultTextBlock.Text = studentList.DisplayStudents(Separator);
         }
     }
 }
