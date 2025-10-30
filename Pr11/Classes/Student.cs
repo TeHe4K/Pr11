82bc7a2e4d11e43ada0cc16a7cd396ef2b6fe022
student-a502 <student-a502@STUD-RM19-A502>
2025-10-30 17:37:58 +0500

    Разработан класс student. Коневский

---
+++ Pr11/Classes/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr11.Classes
+{
+    internal class Student
+    {
+        public string FullName { get; set; }
+        public Student(string FullName)
+        {
+            this.FullName = FullName;
+        }
+        public override string ToString()
+        {
+            return FullName;
+        }
+    }
+}
